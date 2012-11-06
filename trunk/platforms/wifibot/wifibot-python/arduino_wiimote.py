b0ca6ff506dbb009cf2f9a196a5ea6dfcc6e0d0e
Thomas Calmant <thomas.calmant@gmail.com>
2012-11-06 19:13:27 +0100

    Arduino Wiimote updated to use NMEA helper

--- trunk/platforms/wifibot/wifibot-python/arduino_wiimote.py
+++ trunk/platforms/wifibot/wifibot-python/arduino_wiimote.py
@@ -2,26 +2,29 @@
 # -- Content-Encoding: UTF-8 --
 
 from pelix.ipopo.decorators import ComponentFactory, Instantiate, Validate, \
-    Invalidate, Property, Requires, Bind
+    Invalidate, Property, Requires, Bind, Provides
 import pelix.framework as pelix
 
-import io
-import logging
 import serial
 import threading
 
+import logging
 _logger = logging.getLogger(__name__)
-
-WIIMOTE_LISTENER = 'arduino.wiimote.listener'
 
 # ------------------------------------------------------------------------------
 
+SERIAL_LISTENER = 'serial.nmea.listener'
+WIIMOTE_LISTENER = 'arduino.wiimote.listener'
+
 @ComponentFactory("arduino-wiimote-factory")
 @Instantiate("arduino-wiimote")
+@Provides(SERIAL_LISTENER)
 @Requires('_listeners', WIIMOTE_LISTENER, True, True)
-@Property('_threshold', 'data.threshold', 2)
+@Requires('_serial', 'serial.nmea.reader')
+@Property('_nmea_kinds', 'nmea.kinds', '$WICHK')
 @Property('_com_port', 'com.port', '/dev/ttyUSB0')
 @Property('_baud_rate', 'com.baud_rate', 19200)
+@Property('_threshold', 'data.threshold', 2)
 class ArduinoWiimote(object):
     """
     Reads lines from an Arduino
@@ -32,16 +35,14 @@ class ArduinoWiimote(object):
         """
         self._baud_rate = 0
         self._com_port = None
-        self._serial = None
+        self._nmea_kinds = None
 
         self._lock = threading.Lock()
         self._threshold = 0
         self._values = {}
 
+        self._serial = None
         self._listeners = []
-
-        self._stop_event = threading.Event()
-        self._thread = None
 
 
     def __notify_listeners(self, values):
@@ -52,7 +53,7 @@ class ArduinoWiimote(object):
             return
 
         for listener in self._listeners[:]:
-            listener.handleWiimoteValues(self._values.copy())
+            listener.handle_wiimote_values(values)
 
 
     @Bind
@@ -60,85 +61,69 @@ class ArduinoWiimote(object):
         """
         Called when a dependency is bound
         """
-        if svc_ref.get_property(pelix.OBJECTCLASS) == WIIMOTE_LISTENER:
+        if WIIMOTE_LISTENER in svc_ref.get_property(pelix.OBJECTCLASS):
             with self._lock:
                 if self._values:
                     svc.handleWiimoteValues(self._values.copy())
 
 
-    def _parse_line(self, line):
+    def _parse_nmea(self, parts):
         """
         Parses an Arduino Wiimote line:
         
         $WICHK,JOYX,JOYY,ACCX,ACCY,ACCZ,ZBUT,CBUT*CHECKSUM[CR][LF] 
         """
-        parts = line.split('*')[0].split(',')
-        if parts[0] != '$WICHK':
-            # Invalid line
-            return
+        idx = 1
+        needs_update = False
 
+        for key in ('joyX', 'joyY'):
+            new_value = int(parts[idx])
+            cur_value = self._values.get(key, 1000)
+
+            # Biased values
+            if new_value >= 46:
+                new_value -= 46
+
+            else:
+                new_value = 255 - new_value
+
+            if abs(cur_value - new_value) > self._threshold:
+                self._values[key] = new_value
+                needs_update = True
+
+            idx += 1
+
+        for key in ('accX', 'accY', 'accZ'):
+            new_value = int(parts[idx])
+            cur_value = self._values.get(key, 1000)
+            if abs(cur_value - new_value) > self._threshold:
+                self._values[key] = new_value
+                needs_update = True
+
+            idx += 1
+
+        for key in ('Zbut', 'Cbut'):
+            new_value = (int(parts[idx]) != 0)
+            cur_value = self._values.get(key, None)
+            if cur_value != new_value:
+                self._values[key] = new_value
+                needs_update = True
+
+            idx += 1
+
+        return needs_update
+
+
+    def handle_nmea_data(self, nmea_parts):
+        """
+        A NMEA line has been decoded
+        """
         with self._lock:
-            idx = 1
-            needs_update = False
-            value_copy = None
-
-            for key in ('joyX', 'joyY'):
-                new_value = int(parts[idx])
-                cur_value = self._values.get(key, 1000)
-
-                # Biased values
-                if new_value >= 46:
-                    new_value -= 46
-
-                else:
-                    new_value = 255 - new_value
-
-                if abs(cur_value - new_value) > self._threshold:
-                    self._values[key] = new_value
-                    needs_update = True
-
-                idx += 1
-
-            for key in ('accX', 'accY', 'accZ'):
-                new_value = int(parts[idx])
-                cur_value = self._values.get(key, 1000)
-                if abs(cur_value - new_value) > self._threshold:
-                    self._values[key] = new_value
-                    needs_update = True
-
-                idx += 1
-
-            for key in ('Zbut', 'Cbut'):
-                new_value = (int(parts[idx]) != 0)
-                cur_value = self._values.get(key, None)
-                if cur_value != new_value:
-                    self._values[key] = new_value
-                    needs_update = True
-
-                idx += 1
-
-            if needs_update:
-                value_copy = self._values.copy()
+            needs_update = self._parse_nmea(nmea_parts)
+            values_copy = self._values.copy()
 
         if needs_update:
-            self.__notify_listeners(value_copy)
-
-
-    def _read_data(self):
-        """
-        Reads data from the serial port
-        """
-        if self._serial is None:
-            # Nothing to do
-            return
-
-        try:
-            while not self._stop_event.is_set():
-                self._parse_line(self._serial.readline())
-
-        except:
-            _logger.exception("Error reading serial port")
-            self._stop_event.set()
+            self.__notify_listeners(values_copy)
 
 
     def get_data(self):
@@ -159,9 +144,8 @@ class ArduinoWiimote(object):
             self._thread.join()
             self._thread = None
 
-        if self._serial is not None:
-            self._serial.close()
-            self._serial = None
+        # Close the port
+        self._serial.close_port(self._com_port)
 
 
     @Validate
@@ -169,10 +153,5 @@ class ArduinoWiimote(object):
         """
         Component validated
         """
-        self._serial = serial.Serial(self._com_port, int(self._baud_rate))
-        _logger.info("Serial port %s opened at %s", self._com_port,
-                     self._baud_rate)
-
-        self._stop_event.clear()
-        self._thread = threading.Thread(target=self._read_data)
-        self._thread.start()
+        # Open the port
+        self._serial.open_port(self._com_port, int(self._baud_rate))
---
+++ trunk/platforms/wifibot/wifibot-python/wiimote_notifier.py
@@ -0,0 +1,155 @@
+#!/usr/bin/python
+# -- Content-Encoding: UTF-8 --
+
+from pelix.ipopo.decorators import ComponentFactory, Instantiate, Validate, \
+    Invalidate, Property, Requires, Bind, Provides
+import pelix.framework as pelix
+
+import httplib
+import json
+
+import logging
+_logger = logging.getLogger(__name__)
+
+# ------------------------------------------------------------------------------
+
+WIIMOTE_LISTENER = 'arduino.wiimote.listener'
+
+@ComponentFactory("wiimote-notifier-factory")
+@Instantiate("wiimote-notifier")
+@Provides(WIIMOTE_LISTENER)
+@Property('_host', 'robair.controller.server', 'localhost')
+@Property('_port', 'robair.controller.port', 8080)
+@Property('_uri', 'robair.controller.uri', '/control')
+class WiimoteNotifier(object):
+    """
+    Reads lines from an Arduino
+    """
+    def __init__(self):
+        """
+        Sets up the object
+        """
+        self._host = None
+        self._port = 0
+        self._uri = None
+
+    def __compute_speeds(self, joy_x, joy_y, boost):
+        """
+        Computes a tuple of (left, right) speeds
+        """
+        # Normalize the values (bugs over 200)
+        if joy_x > 200:
+            joy_x = 200
+
+        if joy_y > 200:
+            joy_y = 200
+
+        # Compute speed according to X
+        if joy_x < 30:
+            speedL = -2
+            speedR = 2
+
+        elif joy_x < 70:
+            speedL = -1
+            speedR = 2
+
+        elif joy_x > 160:
+            speedL = 2
+            speedR = -2
+
+        elif joy_x > 100:
+            speedL = 2
+            speedR = -1
+
+        else:
+            speedL = 1
+            speedR = 1
+
+
+        if joy_y < 30:
+            ratio = -20
+
+        elif joy_y < 70:
+            ratio = -10
+
+        elif joy_y > 160:
+            ratio = 20
+
+        elif joy_y > 100:
+            ratio = 10
+
+        else:
+            if speedL != 2 and speedR != 2:
+                ratio = 0
+            else:
+                # Robot turns on itself
+                ratio = 10
+
+        # Use the boost!
+        if boost:
+            ratio *= 2
+
+        # Compute the final speed
+        speedL *= ratio
+        speedR *= ratio
+
+        return (speedL, speedR)
+
+
+    def __send_notification(self, values):
+        """
+        Sends a notification over HTTP according to the given Wiimote values
+        """
+        # Connect the server
+        try:
+            conn = httplib.HTTPConnection(self._host, self._port)
+
+        except IOError as ex:
+            _logger.error("Error connecting to [%s]:%s", self._host, self._port)
+            return
+
+        # Prepare the order
+        speedL, speedR = self.__compute_speeds(values['joyX'], values['joyY'],
+                                               values['Zbut'])
+        params = json.dumps({'speedL': speedL, 'speedR': speedR})
+        headers = {
+                   'content-type': 'application/json',
+                   'content-length': len(params),
+                   }
+
+        # Send it
+        try:
+            conn.request('POST', self._uri, params, headers)
+
+            # Get the answer
+            conn.getresponse()
+            conn.close()
+
+        except Exception as ex:
+            _logger.error("Error sending order to [%s]:%s", self._host,
+                          self._port)
+            return
+
+
+    def handle_wiimote_values(self, values):
+        """
+        Notified of new values
+        """
+        self.__send_notification(values)
+
+
+    @Invalidate
+    def invalidate(self, context):
+        """
+        Component invalidated
+        """
+        _logger.info("Wiimote notifier validated")
+
+
+    @Validate
+    def validate(self, context):
+        """
+        Component validated
+        """
+        _logger.info("Wiimote notifier validated")
+
